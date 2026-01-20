453eb8f97b2929066a785265f8e6a3ed85f8d88c
nandiniraut-clg <nandini.raut@cumminscollege.in>
2026-01-20 12:36:38 +0530

    Update and rename linearsearch.java to bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,44 @@
+public class BubbleSort {
+
+    // Method to perform bubble sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                // Compare adjacent elements
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped, array is sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Before sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("\nAfter sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
